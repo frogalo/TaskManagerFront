df49b98985a2bb03194a598cc269abceb680f7ee
Jakub Urbanski <s23525@pjwstk.edu.pl>
2023-06-12 23:23:48 +0200

    updated css for task form

--- src/components/task/TaskForm.js
+++ src/components/task/TaskForm.js
@@ -9,6 +9,7 @@ function TaskForm() {
     const [endDate, setEndDate] = useState('');
     const [status, setStatus] = useState('');
     const [projectId, setProjectId] = useState('');
+    const [isFormValid, setIsFormValid] = useState(false);
 
     useEffect(() => {
         const searchParams = new URLSearchParams(window.location.search);
@@ -17,8 +18,11 @@ function TaskForm() {
     }, []);
 
     const handleFormSubmit = async (event) => {
+        if (!isFormValid) {
+            return; // Exit early if form is not valid
+        }
         event.preventDefault();
-        
+
 
         const task = {
             name: taskName,
@@ -48,65 +52,65 @@ function TaskForm() {
 
     return (
         <div className="task-form-container"> {/* Add className for the container div */}
-            <h2>Add New Task</h2>
-            <form onSubmit={handleFormSubmit} className="task-form"> {/* Add className for the form */}
-                <div>
-                    <label htmlFor="taskName">Task Name:</label>
-                    <input
-                        type="text"
-                        id="taskName"
-                        value={taskName}
-                        onChange={(e) => setTaskName(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="description">Description:</label>
-                    <textarea
-                        id="description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        required
-                    ></textarea>
-                </div>
-                <div>
-                    <label htmlFor="startDate">Start Date:</label>
-                    <input
-                        type="date"
-                        id="startDate"
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="endDate">End Date:</label>
-                    <input
-                        type="date"
-                        id="endDate"
-                        value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="status">Status:</label>
-                    <select
-                        id="status"
-                        value={status}
-                        onChange={(e) => setStatus(e.target.value)}
-                        required
-                    >
-                        <option value="">Select status</option>
-                        <option value="Active">Active</option>
-                        <option value="In Progress">In Progress</option>
-                        <option value="Complete">Complete</option>
-                    </select>
-                </div>
-                <button type="submit" className="add-task-button" onClick={handleFormSubmit}>
-                    Add Task
-                </button>
-            </form>
+            <h2>New Task</h2>
+            <div className="center">
+                <form onSubmit={handleFormSubmit}>
+                    <div className="input-box">
+                        <input type="text"
+                               id="taskName"
+                               value={taskName}
+                               onChange={(e) => setTaskName(e.target.value)}
+                               required
+                        />
+                        <span>Name</span>
+                    </div>
+                    <div className="input-box">
+                        <input type="text"
+                               id="description"
+                               value={description}
+                               onChange={(e) => setDescription(e.target.value)}
+                               required
+                        />
+                        <span>Description</span>
+                    </div>
+                    <div className="inputbox-date">
+                        <label htmlFor="startDate">Start Date:</label>
+                        <input
+                            type="date"
+                            id="startDate"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="inputbox-date">
+                        <label htmlFor="endDate">End Date:</label>
+                        <input
+                            type="date"
+                            id="endDate"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="inputbox-select">
+                        <select
+                            id="status"
+                            value={status}
+                            onChange={(e) => setStatus(e.target.value)}
+                            required
+                        >
+                            <option value="">Select status</option>
+                            <option value="Active">Active</option>
+                            <option value="In Progress">In Progress</option>
+                            <option value="Complete">Complete</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="add-task-button" onClick={handleFormSubmit}>
+                        Add Task
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
